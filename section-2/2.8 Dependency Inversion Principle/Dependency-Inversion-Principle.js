c0523f73474ef821c045b38bf7d2498a1841f8c4
jfkeci <ja.lip132@gmail.com>
2023-08-23 14:00:18 +0200

    DIP

---
+++ section-2/2.8 Dependency Inversion Principle/Dependency-Inversion-Principle.js
@@ -0,0 +1,88 @@
+let Relationship = Object.freeze({
+  parent: 0,
+  child: 1,
+  sibling: 2,
+});
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+/**
+ * LOW LEVEL MODULE
+ * - something that is close to the metal
+ */
+
+class RelationshipBrowser {
+  constructor() {
+    if (this.constructor.name === "RelationshipBrowser")
+      throw new Error("RelationshipBrowser is abstract!");
+  }
+
+  findAllChildrenOf(name) {}
+}
+
+class Relationships extends RelationshipBrowser {
+  constructor() {
+    super();
+    this.data = [];
+  }
+
+  addParentAndChild(parent, child) {
+    this.data.push({
+      from: parent,
+      type: Relationship.parent,
+      to: child,
+    });
+  }
+
+  findAllChildrenOf(name) {
+    return this.data
+      .filter((r) => r.from.name === name && r.type === Relationship.parent)
+      .map((r) => r.to);
+  }
+}
+
+/**
+ * The dependency inversion principle
+ * - High level modules (Research) should directly depend on lovel level modules (Relationships)
+ */
+
+/**
+ * HIGH LEVEL MODULE
+ * - concerned with high level stuff like getting the data out
+ */
+class Research {
+  /** THIS IS THE KEY PROBLEM */
+  // constructor(relationships) {
+  //   // find all children of john
+  //   let relations = relationships.data;
+  //   for (let rel of relations.filter(
+  //     (r) => r.from.name === "John" && r.type === Relationship.parent
+  //   )) {
+  //     console.log(`John has a child named ${rel.to.name}`);
+  //   }
+  // }
+
+  constructor(browser) {
+    for (let p of browser.findAllChildrenOf("John")) {
+      console.log(`John has a child called ${p.name}`);
+    }
+  }
+}
+
+let parent = new Person("John");
+let child1 = new Person("Mary");
+let child2 = new Person("Chris");
+
+let rels = new Relationships();
+rels.addParentAndChild(parent, child1);
+rels.addParentAndChild(parent, child2);
+
+/**
+ * John has a child named Mary
+ * John has a child named Chris
+ * */
+new Research(rels);
